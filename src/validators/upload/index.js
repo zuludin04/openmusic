7fb4f571faa82fec7cabde1d3d9ba30f889e9182
zuludin <zmauludin04@gmail.com>
2024-07-31 01:02:44 +0700

    add upload image validator

---
+++ src/validators/upload/index.js
@@ -0,0 +1,14 @@
+const InvariantError = require("../../exceptions/InvariantError");
+const { ImageHeadersSchema } = require("./scheme");
+
+const UploadsValidator = {
+  validateImageHeaders: (headers) => {
+    const validationResult = ImageHeadersSchema.validate(headers);
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = UploadsValidator;
